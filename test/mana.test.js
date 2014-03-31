05f9d22e8b4a9730867c56a7353433cb2d47eec5
3rd-Eden <info@3rd-Eden.com>
2014-03-31 09:32:21 +0200

    [test] Started on tests for tokens

--- test/mana.test.js
+++ test/mana.test.js
@@ -3,7 +3,10 @@ describe('mana', function () {
 
   var Mana = require('../')
     , chai = require('chai')
-    , expect = chai.expect;
+    , expect = chai.expect
+    , Token = Mana.Token;
+
+  chai.config.includeStack = true;
 
   var mana = new Mana();
 
@@ -83,6 +86,82 @@ describe('mana', function () {
     });
   });
 
+  describe('tokenizer', function () {
+    it('transforms all tokens to Token instances', function () {
+      mana.tokens = ['foo', 'bar'];
+      mana.tokenizer();
+
+      expect(mana.tokens.length).to.equal(2);
+      mana.tokens.forEach(function (token) {
+        expect(token).to.be.instanceOf(Token);
+        expect(/token (foo|bar)/i.test(token.authorization)).to.equal(true);
+      });
+    });
+
+    it('allows multiple invocations', function () {
+      mana.tokens = ['foo', 'bar'];
+      mana.tokenizer().tokenizer().tokens.forEach(function (token) {
+        expect(token).to.be.instanceOf(Token);
+      });
+    });
+
+    it('accepts strings and Token instances', function () {
+      mana.tokens = ['foo', new Token('banana'), 'bar'];
+      mana.tokenizer().tokenizer().tokens.forEach(function (token) {
+        expect(token).to.be.instanceOf(Token);
+      });
+    });
+  });
+
+  describe('#roll', function () {
+    beforeEach(function () {
+      mana.tokens = ['foo'];
+      mana.tokenizer();
+      mana.authorization = mana.tokens[0].authorization;
+
+      mana.ratelimit = 9000;
+      mana.remaining = 10;
+      mana.ratereset = Date.now()/1000;
+    });
+
+    it('resets the current token to the current rate limit', function () {
+      mana.roll();
+      var token = mana.tokens[0];
+
+      expect(token.ratelimit).to.equal(mana.ratelimit);
+      expect(token.remaining).to.equal(mana.remaining);
+      expect(token.ratereset).to.equal(mana.ratereset);
+    });
+
+    it('returns boolean values indicating a working token', function () {
+      expect(mana.roll()).to.equal(true);
+
+      mana.tokens[0].remaining = 0;
+      mana.tokens[0].ratereset = (Date.now() / 1000) + 100;
+      expect(mana.tokens.length).to.equal(1);
+      expect(mana.tokens[0].available()).to.equal(false);
+
+      expect(mana.roll()).to.equal(false);
+    });
+
+    it('returns the most optimal token', function () {
+      mana.tokens = ['foo', 'bar', 'baz', 'oof', 'zab'];
+      mana.tokenizer();
+      mana.tokens.forEach(function (token) {
+        token.ratelimit = 9000;
+        token.remaining = 10;
+        token.ratereset = Date.now()/1000;
+      });
+
+      var token = mana.tokens[2];
+      mana.tokens[1].remaining = token.remaining = 1000;
+      token.ratelimit = 10000;
+
+      expect(mana.roll()).to.equal(true);
+      expect(token.authorization).to.equal(mana.authorization);
+    });
+  });
+
   describe('#downgrade', function () {
     it('calls the function with the first item of the mirror');
     it('continues gets the next mirror on another invocation');
@@ -97,6 +176,8 @@ describe('Tokens', function () {
     , chai = require('chai')
     , expect = chai.expect
     , Token = Mana.Token;
+
+  chai.config.includeStack = true;
 
   var token = new Token('foo')
     , mana = new Mana();
